0e079e8b7139370001935d512265b83ca4b177f0
abrahamarslan <arslan@abrahamarslan.com>
2020-05-29 10:32:43 -0700

    #4 - lodash filter

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {StoursService}  from '../../services/stours.service';
+import {ActivatedRoute} from '@angular/router';
+import _ from 'lodash';
 @Component({
   selector: 'app-list',
   templateUrl: './list.page.html',
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
-
-  constructor() { }
+  tours: any;
+  selection: any;
+  constructor(private _stours: StoursService, private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.selection = this._activatedRoute.snapshot.params;
+    let category = this.selection.Category;
+    let criteria = this.selection.Criteria;
+    this.tours = _.filter(this._stours.tours, [category, criteria]);
   }
 
 }
--- src/app/services/stours.service.ts
+++ src/app/services/stours.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import _ from 'lodash';
 @Injectable({
   providedIn: 'root'
 })
 export class StoursService {
   public regions: any;
   public tourTypes: any;
+  public tours: any;
   baseURL:string = 'https://tours-firebase.firebaseio.com';
   constructor(private _http: HttpClient) { }
 
@@ -17,7 +19,9 @@ export class StoursService {
    	this.getRegions()
    		.then(data => this.regions = data);
 	this.getTourTypes()
-		.then(data => this.tourTypes = data);
+		.then(data => this.tourTypes = _.sortBy(data, 'Name'));
+	this.getTours()
+		.then(data => this.tours = _.sortBy(data, 'Title'));
    }
 
   /**
@@ -37,6 +41,18 @@ export class StoursService {
 	 */
 	 getTourTypes() {
 	 	let requestURL = `${this.baseURL}/Tourtypes.json`;
+	 	return this._http.get(requestURL).toPromise();
+	 }
+
+	/**
+	 *
+	 * * Get tours
+	 * @param {}
+	 * @return {<Promise>}
+	 *
+	 */
+	getTours() {
+	 	let requestURL = `${this.baseURL}/Tours.json`;
 	 	return this._http.get(requestURL).toPromise();
 	 }
 }
